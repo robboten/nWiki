f0a395e87c2f5205195700740156e840af488937
robboten <robboten@gmail.com>
2023-02-16 15:29:18 +0100

    Api start

---
+++ Wiki.Api/Character.cs
@@ -0,0 +1,17 @@
+﻿namespace Wiki.Api
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Birthday { get; set; }
+        public int Age { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public string Quote { get; set; } = string.Empty;
+        public string PortraitUrl { get; set; } = string.Empty;
+        //public List<string> Aliases { get; set; }
+        //public List<string> Species { get; set; }
+        //public List<Character> Family { get; set; }
+        //public List<Character> Relationship { get; set; }
+    }
+}
---
+++ Wiki.Api/Controllers/CharactersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wiki.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharactersController : ControllerBase
+    {
+        private readonly List<Character> characters = new();
+        public CharactersController()
+        {
+            characters = new List<Character>()
+            {
+                new Character()
+                {
+                    Id= 1,
+                    Name="Zev",
+                    Age=23,
+                    Quote="Integer vehicula vitae velit quis semper. Aliquam porta erat sit amet aliquam lobortis. Vivamus et ligula eget justo pharetra sagittis. Fusce lorem ipsum, sagittis quis turpis a, maximus auctor ipsum.",
+                    PortraitUrl="https://github.com/NoahFerm/Wiki1.0/blob/main/Wiki/bilder/zev.png?raw=true",
+
+                },
+                new Character()
+                {
+                    Id= 2,
+                    Name="Alex",
+                    Age=24,
+                    Quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et arcu et est semper molestie at sit amet urna. Integer eget aliquam sapien, sed tempor velit. ",
+                    PortraitUrl="https://github.com/NoahFerm/Wiki1.0/blob/main/Wiki/bilder/alex.png?raw=true",
+                }
+            };
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Character>> GetCharactersAsync()
+        {
+            return characters;
+        }
+
+    }
+}
