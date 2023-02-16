78402c45828f59310364b66667ab7c32aa5a0d8a
Aeliyan <90610537+NoahFerm@users.noreply.github.com>
2023-02-16 09:52:38 +0100

    Character class

--- Wiki.App/Entities/Character.cs
+++ Wiki.App/Entities/Character.cs
@@ -2,5 +2,14 @@
 {
     public class Character
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Age { get; set; }
+        public string Status { get; set; }
+        //public List<string> Aliases { get; set; }
+        //public List<string> Species { get; set; }
+        //public List<Character> Family { get; set; }
+        //public List<Character> Relationship { get; set; }
     }
 }
