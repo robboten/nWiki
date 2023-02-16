1cfd983becf810c21095b9811216028d3870fed7
robboten <robboten@gmail.com>
2023-02-16 10:01:30 +0100

    basic mock

--- Wiki.App/Entities/Character.cs
+++ Wiki.App/Entities/Character.cs
@@ -3,10 +3,10 @@
     public class Character
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public DateTime Birthday { get; set; }
         public int Age { get; set; }
-        public string Status { get; set; }
+        public string Status { get; set; } = string.Empty;
         //public List<string> Aliases { get; set; }
         //public List<string> Species { get; set; }
         //public List<Character> Family { get; set; }
---
+++ Wiki.App/Services/MockClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using Wiki.App.Entities;
+
+namespace Wiki.App.Services
+{
+    public class MockClient
+    {
+        private readonly HttpClient _httpClient;
+        public MockClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Character>> GetCharactersAsync()
+        {
+            var result = new List<Character>()
+            {
+                new Character()
+                {
+                    Id= 1,
+                    Name="Zev",
+                    Age=23
+                }
+            };
+            return result;
+        }
+    }
+}
